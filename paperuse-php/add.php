745ae2923950374c1b7de922bd0bbcaabdc84d83
zhouhaoyuan123 <s3462599202@qq.com>
2023-03-03 16:51:52 +0800

    update
    sql update

--- paperuse-php/add.php
+++ paperuse-php/add.php
@@ -5,12 +5,16 @@ if(!isset($_SESSION["user"]) || !isset($_SESSION["password"])){
 }   
 $ntime = date("Y/m/d");
 $sql=sprintf("INSERT INTO `records` ('usr','date','value') VALUES ('%s','%s',%s);",$_SESSION["user"],$ntime,$_POST["record"]);
-$sql2 = sprintf("SELECT * FROM `records` WHERE usr='%s',date='%s';",$_SESSION["user"],$ntime);
+$sql2 = sprintf("SELECT * FROM `records` WHERE usr='%s' AND date='%s';",$_SESSION["user"],$ntime);
 $sql3 = sprintf("UPDATE records SET value = '%s' WHERE usr = '%s' AND date = '%s';",$_POST["record"],$_SESSION["user"],$ntime);
 $host = "127.0.0.1";
 $duser = "root";
 $dpasswd = "";
 $db = "php" ;
+echo $sql.'/n';
+echo $sql2.'/n';
+echo $sql3.'/n';
+//\0 
 $conn=new mysqli($host,$duser,$dpasswd,$db);
 $res1 = $conn->query($sql2);
 if($res1 && $res1->num_rows>0) {
